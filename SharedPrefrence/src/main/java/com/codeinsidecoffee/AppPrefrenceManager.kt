1edb5d6de7392af6204ca04b40e7bf6132af9d95
nmpebizzinfotech <78020366+nmpebizzinfotech@users.noreply.github.com>
2021-02-15 17:15:05 +0530

    Allow user to store double , int, string ,boolean,set<String> ,float and Long value using single class file.

---
+++ SharedPrefrence/src/main/java/com/codeinsidecoffee/AppPrefrenceManager.kt
@@ -0,0 +1,40 @@
+package com.codeinsidecoffee
+
+import android.content.SharedPreferences
+
+class AppPrefrenceManager(var mSharedPreferences: SharedPreferences) {
+    fun setValue(key:String,value:Any){
+        val editor=mSharedPreferences.edit()
+        when(value){
+            is String -> editor.putString(key,value) // TODO - Use to store String Value
+            is Int -> editor.putInt(key,value)  // TODO - Use to store Integer Value
+            is Long -> editor.putLong(key,value)  // TODO - Use to store Long Value
+            is Float -> editor.putFloat(key,value)  // TODO - Use to store Float Value
+            is Double -> editor.putLong(key,value.toBits())  // TODO - Use to store Double Value
+            is Boolean -> editor.putBoolean(key,value)  // TODO - Use to store Boolean Value
+            is Set<*> -> editor.putStringSet(key,value as Set<String>) // TODO - Use to store Set<String> Value
+        }
+        editor.apply()
+    }
+
+    fun getValue(key:String,defaultvalue:Any): Any? {
+        when (defaultvalue){
+            is String -> return mSharedPreferences.getString(key,defaultvalue) as String // TODO - Use to retrive String Value
+            is Int -> return mSharedPreferences.getInt(key,defaultvalue) as Int // TODO - Use to retrive Integer Value
+            is Long -> return mSharedPreferences.getLong(key,defaultvalue) as Long  // TODO - Use to retrive Long Value
+            is Float -> return mSharedPreferences.getFloat(key,defaultvalue) as Float  // TODO - Use to retrive Float Value
+            is Double -> return Double.fromBits(mSharedPreferences.getLong(key,(defaultvalue).toBits()) as Long)  // TODO - Use to retrive Double Value
+            is Boolean -> return mSharedPreferences.getBoolean(key,defaultvalue) as Boolean  // TODO - Use to retrive Boolean Value
+            is Set<*> -> return mSharedPreferences.getStringSet(key,defaultvalue as Set<String>) as Set<String>  // TODO - Use to retrive Set<String> Value
+        }
+        return null
+    }
+
+    fun clear(){ // TODO - it clear Prefrence Value
+        mSharedPreferences.edit().clear().apply()
+    }
+
+    fun remove(key:String){  // TODO - helps to remove particular value based on prefrence keyname
+        mSharedPreferences.edit().remove(key).apply()
+    }
+}
---
+++ app/src/main/java/com/codeinsidecoffee/sharedprefrencemanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codeinsidecoffee.sharedprefrencemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
